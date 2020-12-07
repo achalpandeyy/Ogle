166c4d3b7080405ca9d8819621776b8c4529ba4f
achalpandeyy <pandeyachal.ap@gmail.com>
2020-12-07 22:33:34 +0530

    FPS Camera

--- Ogle/Source/Main.cpp
+++ Ogle/Source/Main.cpp
@@ -11,9 +11,63 @@
 #include <fstream>
 #include <sstream>
 
-void FramebufferSizeCallback(GLFWwindow* window, int width, int height)
+glm::vec3 camera_position(0.f, 0.f, 3.f);
+glm::vec3 camera_front = glm::vec3(0.f, 0.f, -1.f);
+glm::vec3 camera_orientation(0.f, 1.f, 0.f);
+
+int width = 1280;
+int height = 720;
+
+float delta_time = 0.f;
+float last_frame = 0.f;
+
+double last_x = (double)width / 2;
+double last_y = (double)height / 2;
+float yaw = -90.f;
+float pitch = 0.f;
+float camera_fov = 45.f;
+bool first_mouse = true;
+
+void GLFWFramebufferSizeCallback(GLFWwindow* window, int width, int height)
 {
     glViewport(0, 0, width, height);
+}
+
+void GLFWMouseCallback(GLFWwindow* window, double x_pos, double y_pos)
+{
+    if (first_mouse)
+    {
+        last_x = x_pos;
+        last_y = y_pos;
+        first_mouse = false;
+    }
+
+    float x_offset = (float)(x_pos - last_x);
+    float y_offset = (float)(last_y - y_pos);
+
+    const float sensitivity = 0.05f;
+    x_offset *= sensitivity;
+    y_offset *= sensitivity;
+
+    yaw += x_offset;
+    pitch += y_offset;
+
+    pitch = glm::clamp(pitch, -89.f, 89.f);
+
+    camera_front.x = glm::cos(glm::radians(pitch)) * glm::cos(glm::radians(yaw));
+    camera_front.y = glm::sin(glm::radians(pitch));
+    camera_front.z = glm::cos(glm::radians(pitch)) * glm::sin(glm::radians(yaw));
+
+    camera_front = glm::normalize(camera_front);
+
+    last_x = x_pos;
+    last_y = y_pos;
+}
+
+void GLFWScrollCallback(GLFWwindow* window, double x_offset, double y_offset)
+{
+    camera_fov -= y_offset;
+    camera_fov = glm::clamp(camera_fov, 1.f, 45.f);
 }
 
 #ifdef _DEBUG
@@ -71,8 +125,6 @@ int main()
     if (!glfwInit())
         return -1;
 
-    int width = 1280;
-    int height = 720;
 
 #ifdef _DEBUG
     glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, true);
@@ -87,16 +139,59 @@ int main()
 
     glfwSetWindowTitle(window, "Ogle");
 
-    glfwSetFramebufferSizeCallback(window, FramebufferSizeCallback);
+    glfwSetFramebufferSizeCallback(window, GLFWFramebufferSizeCallback);
+    glfwSetCursorPosCallback(window, GLFWMouseCallback);
+    glfwSetScrollCallback(window, GLFWScrollCallback);
+
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 
     glfwMakeContextCurrent(window);
 
+    // Todo: You can use the GL_TRIANGLE_STRIP primitive, that way, you won't need
+    // to store this many vertices for a cube
     float vertices[] =
     {
-        -0.5f, -0.5f, -0.f,  0.f, 0.f,
-         0.5f, -0.5f, -0.f,  1.f, 0.f,
-         0.5f,  0.5f, -0.f,  1.f, 1.f,
-        -0.5f,  0.5f, -0.f,  0.f, 1.f
+        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+         0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+         0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+        -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        -0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+        -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+         0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+         0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+         0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+         0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+         0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+         0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
     };
 
     unsigned int indices[] = { 0, 1, 2, 2, 3, 0 };
@@ -106,6 +201,8 @@ int main()
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
+
+    glEnable(GL_DEPTH_TEST);
 
 #ifdef _DEBUG
     {
@@ -146,11 +243,6 @@ int main()
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    GLuint ibo;
-    glGenBuffers(1, &ibo);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
     GLsizei stride = 5 * sizeof(float);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, stride, (const void*)0);
     glEnableVertexAttribArray(0);
@@ -176,7 +268,7 @@ int main()
 
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, tex_width, tex_height, 0, GL_RGB, GL_UNSIGNED_BYTE, tex_data);
@@ -189,30 +281,68 @@ int main()
     Shader shader("Source/Shaders/Shader.vert", "Source/Shaders/Shader.frag");
     shader.Bind();
 
-    glm::mat4 model = glm::translate(glm::mat4(1.f), glm::vec3(0.f, 0.f, -2.f));
-    model = glm::rotate(model, glm::radians(55.f), glm::vec3(-1.f, 0.f, 0.f));
-    glm::mat4 proj = glm::perspective(glm::radians(45.f), (float)width / (float)height, 0.1f, 100.f);
+    glm::mat4 model = glm::mat4(1.f);
 
-    glm::mat4 pvm = proj * model;
+    glm::mat4 view = glm::translate(glm::mat4(1.f), -camera_position);
+    glm::mat4 proj = glm::perspective(glm::radians(camera_fov), (float)width / (float)height, 0.1f, 100.f);
+
+    glm::mat4 pvm = proj * view * model;
     shader.SetMat4("pvm", glm::value_ptr(pvm), false);
 
     while (!glfwWindowShouldClose(window))
     {
+        float current_frame = (float)glfwGetTime();
+        delta_time = current_frame - last_frame;
+        last_frame = current_frame;
+
+        if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        {
+            glfwSetWindowShouldClose(window, GLFW_TRUE);
+        }
+
+        const float camera_speed = 2.5f * delta_time;
+        if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+        {
+            camera_position += camera_speed * camera_front;
+        }
+
+        if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+        {
+            camera_position -= camera_speed * camera_front;
+        }
+
+        glm::vec3 camera_right = glm::normalize(glm::cross(camera_front, camera_orientation));
+        if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+        {
+            camera_position -= camera_speed * camera_right;
+        }
+
+        if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+        {
+            camera_position += camera_speed * camera_right;
+        }       
+
+        glfwPollEvents();
+
+        view = glm::lookAt(camera_position, camera_position + camera_front, camera_orientation);
+        proj = glm::perspective(glm::radians(camera_fov), (float)width / (float)height, 0.1f, 100.f);
+        pvm = proj * view * model;
+
+        shader.Bind();
+        shader.SetMat4("pvm", glm::value_ptr(pvm), false);
+
         glClearColor(0.1f, 0.1f, 0.1f, 1.f);
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         glBindVertexArray(vao);
         glBindTexture(GL_TEXTURE_2D, texture);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glDrawArrays(GL_TRIANGLES, 0, 36);
 
         glfwSwapBuffers(window);
-
-        glfwPollEvents();
     }
 
     glDeleteVertexArrays(1, &vao);
     glDeleteBuffers(1, &vbo);
-    glDeleteBuffers(1, &ibo);
     glDeleteTextures(1, &texture);
 
     glfwTerminate();
