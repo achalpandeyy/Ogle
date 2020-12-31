749b6dd9bd390b31a7c213ddd2fa02b193029f22
achalpandeyy <pandeyachal.ap@gmail.com>
2020-12-31 14:40:30 +0530

    Add an Image Processing (currently only displays) test, alongwith some cleanups

--- Ogle/Source/Main.cpp
+++ Ogle/Source/Main.cpp
@@ -11,22 +11,22 @@
 #include <fstream>
 #include <sstream>
 
-glm::vec3 camera_position(3.f, 2.f, 7.f);
-glm::vec3 camera_front = glm::vec3(0.f, 0.f, -1.f);
-glm::vec3 camera_orientation(0.f, 1.f, 0.f);
+glm::vec3 g_camera_position(3.f, 2.f, 7.f);
+glm::vec3 g_camera_front = glm::vec3(0.f, 0.f, -1.f);
+glm::vec3 g_camera_orientation(0.f, 1.f, 0.f);
 
-int width = 1280;
-int height = 720;
+int g_width = 1280;
+int g_height = 720;
 
-float delta_time = 0.f;
-float last_frame = 0.f;
+float g_delta_time = 0.f;
+float g_last_frame = 0.f;
 
-double last_x = (double)width / 2;
-double last_y = (double)height / 2;
-float yaw = -90.f;
-float pitch = 0.f;
-float camera_fov = 60.f;
-bool first_mouse = true;
+double g_last_x = (double)g_width / 2;
+double g_last_y = (double)g_height / 2;
+float g_yaw = -90.f;
+float g_pitch = 0.f;
+float g_camera_fov = 60.f;
+bool g_first_mouse = true;
 
 void GLFWFramebufferSizeCallback(GLFWwindow* window, int width, int height)
 {
@@ -35,41 +35,74 @@ void GLFWFramebufferSizeCallback(GLFWwindow* window, int width, int height)
 
 void GLFWMouseCallback(GLFWwindow* window, double x_pos, double y_pos)
 {
-    if (first_mouse)
+    if (g_first_mouse)
     {
-        last_x = x_pos;
-        last_y = y_pos;
-        first_mouse = false;
+        g_last_x = x_pos;
+        g_last_y = y_pos;
+        g_first_mouse = false;
     }
 
-    float x_offset = (float)(x_pos - last_x);
-    float y_offset = (float)(last_y - y_pos);
+    float x_offset = (float)(x_pos - g_last_x);
+    float y_offset = (float)(g_last_y - y_pos);
 
     const float sensitivity = 0.05f;
     x_offset *= sensitivity;
     y_offset *= sensitivity;
 
-    yaw += x_offset;
-    pitch += y_offset;
+    g_yaw += x_offset;
+    g_pitch += y_offset;
 
-    pitch = glm::clamp(pitch, -89.f, 89.f);
+    g_pitch = glm::clamp(g_pitch, -89.f, 89.f);
 
-    camera_front.x = glm::cos(glm::radians(pitch)) * glm::cos(glm::radians(yaw));
-    camera_front.y = glm::sin(glm::radians(pitch));
-    camera_front.z = glm::cos(glm::radians(pitch)) * glm::sin(glm::radians(yaw));
+    g_camera_front.x = glm::cos(glm::radians(g_pitch)) * glm::cos(glm::radians(g_yaw));
+    g_camera_front.y = glm::sin(glm::radians(g_pitch));
+    g_camera_front.z = glm::cos(glm::radians(g_pitch)) * glm::sin(glm::radians(g_yaw));
 
-    camera_front = glm::normalize(camera_front);
+    g_camera_front = glm::normalize(g_camera_front);
 
-    last_x = x_pos;
-    last_y = y_pos;
+    g_last_x = x_pos;
+    g_last_y = y_pos;
 }
 
 void GLFWScrollCallback(GLFWwindow* window, double x_offset, double y_offset)
 {
-    camera_fov -= (float)y_offset;
-    camera_fov = glm::clamp(camera_fov, 1.f, 45.f);
+    g_camera_fov -= (float)y_offset;
+    g_camera_fov = glm::clamp(g_camera_fov, 1.f, 45.f);
 }
 
+bool g_show_image_test = false;
+void GLFWKeyCallback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    switch (key)
+    {
+        case GLFW_KEY_ESCAPE:
+        {
+            glfwSetWindowShouldClose(window, GLFW_TRUE);
+        } break;
+
+        case GLFW_KEY_Q:
+        {
+            glfwSetCursorPosCallback(window, 0);
+            glfwSetScrollCallback(window, 0);
+
+            glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
+
+            g_show_image_test = true;
+        } break;
+
+        case GLFW_KEY_E:
+        {
+            glfwSetCursorPosCallback(window, GLFWMouseCallback);
+            glfwSetScrollCallback(window, GLFWScrollCallback);
+
+            glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+            g_first_mouse = true;
+
+            g_show_image_test = false;
+        } break;
+    }
+}
 
 #ifdef _DEBUG
 void WINAPI GLDebugOutput(GLenum source, GLenum type, unsigned int id, GLenum severity, GLsizei length,
@@ -119,18 +152,61 @@ void WINAPI GLDebugOutput(GLenum source, GLenum type, unsigned int id, GLenum se
 }
 #endif
 
+struct Mesh
+{
+    Mesh(const float* vertices, unsigned int vertex_count, const unsigned int* indices, unsigned int index_count)
+    {
+        glGenVertexArrays(1, &vao);
+        BindVAO();
+
+        glGenBuffers(1, &vbo);
+        glBindBuffer(GL_ARRAY_BUFFER, vbo);
+        glBufferData(GL_ARRAY_BUFFER, vertex_count * sizeof(float), vertices, GL_STATIC_DRAW);
+
+        glGenBuffers(1, &ibo);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, index_count * sizeof(unsigned int), indices, GL_STATIC_DRAW);
+
+        glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
+        glEnableVertexAttribArray(0);
+
+        UnbindVAO();
+    }
+
+    ~Mesh()
+    {
+        glDeleteVertexArrays(1, &vao);
+        glDeleteBuffers(1, &vbo);
+        glDeleteBuffers(1, &ibo);
+    }
+
+    inline void BindVAO() const { glBindVertexArray(vao); }
+    inline void UnbindVAO() const { glBindVertexArray(0); }
+
+private:
+    GLuint vao;
+    GLuint vbo;
+    GLuint ibo;
+};
+
+glm::vec3 GetRayDirectionFromNDC(const glm::vec2& ndc, const glm::mat4& view_proj_inverse, const glm::vec3& ray_origin)
+{
+    glm::vec4 point_world_space = view_proj_inverse * glm::vec4(ndc, 0.f, 1.f);
+    point_world_space /= point_world_space.w;
+    return glm::vec3(point_world_space) - ray_origin;
+}
+
 int main()
 {
     GLFWwindow* window;
 
-    if (!glfwInit())
-        return -1;
+    if (!glfwInit()) return -1;
 
 #ifdef _DEBUG
     glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, true);
 #endif
 
-    window = glfwCreateWindow(width, height, "Ogle", NULL, NULL);
+    window = glfwCreateWindow(g_width, g_height, "Ogle", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
@@ -138,15 +214,16 @@ int main()
     }
 
     glfwSetWindowTitle(window, "Ogle");
-
+    
     glfwSetFramebufferSizeCallback(window, GLFWFramebufferSizeCallback);
+
     glfwSetCursorPosCallback(window, GLFWMouseCallback);
     glfwSetScrollCallback(window, GLFWScrollCallback);
+    glfwSetKeyCallback(window, GLFWKeyCallback);
 
     glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 
     glfwMakeContextCurrent(window);
-
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
     {
         std::cout << "Failed to initialize GLAD" << std::endl;
@@ -159,12 +236,12 @@ int main()
         glGetIntegerv(GL_CONTEXT_FLAGS, &flags);
         if (flags & GL_CONTEXT_FLAG_DEBUG_BIT)
         {
-            std::cout << "Note: Debug context initialized\n" << std::endl;
-
             glEnable(GL_DEBUG_OUTPUT);
             glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
             glDebugMessageCallback(GLDebugOutput, nullptr);
             glDebugMessageControl(GL_DONT_CARE, GL_DONT_CARE, GL_DONT_CARE, 0, nullptr, GL_TRUE);
+
+            std::cout << "Note: Debug context initialized\n" << std::endl;
         }
     }
 #endif
@@ -199,20 +276,7 @@ int main()
         std::cout << "Max Work Group Invocations: " << max_work_group_invocations << std::endl;
     }
 
-    glViewport(0, 0, width, height);
-
-    GLuint fb_texture;
-    glGenTextures(1, &fb_texture);
-    glBindTexture(GL_TEXTURE_2D, fb_texture);
-
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA32F, width, height, 0, GL_RGBA, GL_FLOAT, 0);
-
-    glBindTexture(GL_TEXTURE_2D, 0);
+    glViewport(0, 0, g_width, g_height);
 
     float quad_vertices[] =
     {
@@ -224,140 +288,173 @@ int main()
 
     unsigned int quad_indices[] = { 0, 1, 2, 2, 3, 0 };
 
-    GLuint quad_vao;
-    glGenVertexArrays(1, &quad_vao);
-    glBindVertexArray(quad_vao);
+    Mesh fullscreen_quad(quad_vertices, 8 * 2, quad_indices, 6);
 
-    GLuint quad_vbo;
-    glGenBuffers(1, &quad_vbo);
-    glBindBuffer(GL_ARRAY_BUFFER, quad_vbo);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(quad_vertices), quad_vertices, GL_STATIC_DRAW);
+    GLuint fb_texture;
+    glGenTextures(1, &fb_texture);
+    glBindTexture(GL_TEXTURE_2D, fb_texture);
 
-    GLuint quad_ibo;
-    glGenBuffers(1, &quad_ibo);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, quad_ibo);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(quad_indices), quad_indices, GL_STATIC_DRAW);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
-    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA32F, g_width, g_height, 0, GL_RGBA, GL_FLOAT, 0);
 
-    glBindVertexArray(0);
+    glBindTexture(GL_TEXTURE_2D, 0);
+
+    Shader fullscreen_quad_shader("Source/Shaders/FullscreenQuadShader.vert", "Source/Shaders/FullscreenQuadShader.frag");
 
     Shader raytracing_shader("Source/Shaders/Raytracing.comp");
-    Shader quad_shader("Source/Shaders/QuadShader.vert", "Source/Shaders/QuadShader.frag");
 
-    GLint work_group_size[3] = {};
-    glGetProgramiv(raytracing_shader.id, GL_COMPUTE_WORK_GROUP_SIZE, work_group_size);
-
-    glm::mat4 view = glm::translate(glm::mat4(1.f), -camera_position);
-    glm::mat4 proj = glm::perspective(glm::radians(camera_fov), (float)width / (float)height, 0.1f, 100.f);
+    glm::mat4 view = glm::translate(glm::mat4(1.f), -g_camera_position);
+    glm::mat4 proj = glm::perspective(glm::radians(g_camera_fov), (float)g_width / (float)g_height, 0.1f, 100.f);
 
     glm::mat4 view_proj_inv = glm::inverse(view)* glm::inverse(proj);
 
     raytracing_shader.Bind();
 
-    raytracing_shader.SetVec3("u_Eye", camera_position.x, camera_position.y, camera_position.z);
+    raytracing_shader.SetVec3("u_Eye", g_camera_position.x, g_camera_position.y, g_camera_position.z);
 
-    glm::vec4 ray00 = view_proj_inv * glm::vec4(-1.f, -1.f, 0.f, 1.f);
-    ray00 /= ray00.w;
-    raytracing_shader.SetVec3("u_Ray00", ray00.x - camera_position.x, ray00.y - camera_position.y, ray00.z - camera_position.z);
+    glm::vec3 ray_direction00 = GetRayDirectionFromNDC(glm::vec2(-1.f, -1.f), view_proj_inv, g_camera_position);
+    raytracing_shader.SetVec3("u_RayDirection00", ray_direction00.x, ray_direction00.y, ray_direction00.z);
 
-    glm::vec4 ray01 = view_proj_inv * glm::vec4(-1.f, 1.f, 0.f, 1.f);
-    ray01 /= ray01.w;
-    raytracing_shader.SetVec3("u_Ray01", ray01.x - camera_position.x, ray01.y - camera_position.y, ray01.z - camera_position.z);
+    glm::vec3 ray_direction01 = GetRayDirectionFromNDC(glm::vec2(-1.f, 1.f), view_proj_inv, g_camera_position);
+    raytracing_shader.SetVec3("u_RayDirection01", ray_direction01.x, ray_direction01.y, ray_direction01.z);
 
-    glm::vec4 ray10 = view_proj_inv * glm::vec4(1.f, -1.f, 0.f, 1.f);
-    ray10 /= ray10.w;
-    raytracing_shader.SetVec3("u_Ray10", ray10.x - camera_position.x, ray10.y - camera_position.y, ray10.z - camera_position.z);
+    glm::vec3 ray_direction10 = GetRayDirectionFromNDC(glm::vec2(1.f, -1.f), view_proj_inv, g_camera_position);
+    raytracing_shader.SetVec3("u_RayDirection10", ray_direction10.x, ray_direction10.y, ray_direction10.z);
 
-    glm::vec4 ray11 = view_proj_inv * glm::vec4(1.f, 1.f, 0.f, 1.f);
-    ray11 /= ray11.w;
-    raytracing_shader.SetVec3("u_Ray11", ray11.x - camera_position.x, ray11.y - camera_position.y, ray11.z - camera_position.z);
+    glm::vec3 ray_direction11 = GetRayDirectionFromNDC(glm::vec2(1.f, 1.f), view_proj_inv, g_camera_position);
+    raytracing_shader.SetVec3("u_RayDirection11", ray_direction11.x, ray_direction11.y, ray_direction11.z);
+
+    GLint work_group_size[3] = {};
+    glGetProgramiv(raytracing_shader.id, GL_COMPUTE_WORK_GROUP_SIZE, work_group_size);
+
+    glClearColor(0.1f, 0.1f, 0.1f, 1.f);
+
+    // Image Processing Test
+    GLuint image_tex = GLuint(-1);
 
     while (!glfwWindowShouldClose(window))
     {
+        // Todo: For the image processing test I don't need delta_time
         float current_frame = (float)glfwGetTime();
-        delta_time = current_frame - last_frame;
-        last_frame = current_frame;
+        g_delta_time = current_frame - g_last_frame;
+        g_last_frame = current_frame;
 
-        if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-        {
-            glfwSetWindowShouldClose(window, GLFW_TRUE);
-        }
-
-        const float camera_speed = 2.5f * delta_time;
+        // Todo: For the image processing test I don't need this camera calculation,
+        // there is no notion of a camera
+        const float camera_speed = 2.5f * g_delta_time;
         if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
         {
-            camera_position += camera_speed * camera_front;
+            g_camera_position += camera_speed * g_camera_front;
         }
 
         if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
         {
-            camera_position -= camera_speed * camera_front;
+            g_camera_position -= camera_speed * g_camera_front;
         }
 
-        glm::vec3 camera_right = glm::normalize(glm::cross(camera_front, camera_orientation));
+        glm::vec3 camera_right = glm::normalize(glm::cross(g_camera_front, g_camera_orientation));
         if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
         {
-            camera_position -= camera_speed * camera_right;
+            g_camera_position -= camera_speed * camera_right;
         }
 
         if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
         {
-            camera_position += camera_speed * camera_right;
+            g_camera_position += camera_speed * camera_right;
         }
 
         glfwPollEvents();
 
-        view = glm::lookAt(camera_position, camera_position + camera_front, camera_orientation);
-        proj = glm::perspective(glm::radians(camera_fov), (float)width / (float)height, 0.1f, 100.f);
+        glClear(GL_COLOR_BUFFER_BIT);
 
-        glm::mat4 view_proj_inv = glm::inverse(view) * glm::inverse(proj);
+        if (g_show_image_test)
+        {
+            glClearColor(0.8f, 0.3f, 0.2f, 1.f);
+            glClear(GL_COLOR_BUFFER_BIT);
 
-        raytracing_shader.Bind();
+            if (image_tex == GLuint(-1))
+            {
+                const char* image_path = "../Resources/container.jpg";
+                int image_tex_width, image_tex_height, image_tex_channel_count;
+                unsigned char* image_data = stbi_load(image_path, &image_tex_width, &image_tex_height, &image_tex_channel_count, 3);
 
-        raytracing_shader.SetVec3("u_Eye", camera_position.x, camera_position.y, camera_position.z);
+                if (image_data)
+                {
+                    glGenTextures(1, &image_tex);
+                    glBindTexture(GL_TEXTURE_2D, image_tex);
 
-        ray00 = view_proj_inv * glm::vec4(-1.f, -1.f, 0.f, 1.f);
-        ray00 /= ray00.w;
-        raytracing_shader.SetVec3("u_Ray00", ray00.x - camera_position.x, ray00.y - camera_position.y, ray00.z - camera_position.z);
+                    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+                    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+                    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+                    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
-        ray01 = view_proj_inv * glm::vec4(-1.f, 1.f, 0.f, 1.f);
-        ray01 /= ray01.w;
-        raytracing_shader.SetVec3("u_Ray01", ray01.x - camera_position.x, ray01.y - camera_position.y, ray01.z - camera_position.z);
+                    // Todo: You can make this an immutable texture
+                    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, image_tex_width, image_tex_height, 0, GL_RGB, GL_UNSIGNED_BYTE, image_data);
 
-        ray10 = view_proj_inv * glm::vec4(1.f, -1.f, 0.f, 1.f);
-        ray10 /= ray10.w;
-        raytracing_shader.SetVec3("u_Ray10", ray10.x - camera_position.x, ray10.y - camera_position.y, ray10.z - camera_position.z);
+                    glBindTexture(GL_TEXTURE_2D, 0);
+                }
+                else
+                {
+                    std::cout << "Failed to load image: " << image_path << std::endl;
+                    exit(1);
+                }
 
-        ray11 = view_proj_inv * glm::vec4(1.f, 1.f, 0.f, 1.f);
-        ray11 /= ray11.w;
-        raytracing_shader.SetVec3("u_Ray11", ray11.x - camera_position.x, ray11.y - camera_position.y, ray11.z - camera_position.z);
+                stbi_image_free(image_data);
+            }
 
-        // Bind level 0 of the framebuffer texture to image binding point 0
-        glBindImageTexture(0, fb_texture, 0, GL_FALSE, 0, GL_WRITE_ONLY, GL_RGBA32F);
+            glBindTexture(GL_TEXTURE_2D, image_tex);
 
-        // Todo: Shoule width and height be a power of two?
-        // Dispatch the compute shader to generate a frame in the framebuffer image
-        glDispatchCompute(width / work_group_size[0], height / work_group_size[1], 1);
+            fullscreen_quad.BindVAO();
+            fullscreen_quad_shader.Bind();
+            glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        }
+        else
+        {
+            view = glm::lookAt(g_camera_position, g_camera_position + g_camera_front, g_camera_orientation);
+            proj = glm::perspective(glm::radians(g_camera_fov), (float)g_width / (float)g_height, 0.1f, 100.f);
 
-        // Unbind image binding point
-        glBindImageTexture(0, 0, 0, GL_FALSE, 0, GL_WRITE_ONLY, GL_RGBA32F);
+            glm::mat4 view_proj_inv = glm::inverse(view) * glm::inverse(proj);
 
-        glActiveTexture(GL_TEXTURE0);
-        glBindTexture(GL_TEXTURE_2D, fb_texture);
+            raytracing_shader.Bind();
 
-        quad_shader.Bind();
-        glBindVertexArray(quad_vao);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+            raytracing_shader.SetVec3("u_Eye", g_camera_position.x, g_camera_position.y, g_camera_position.z);
+
+            ray_direction00 = GetRayDirectionFromNDC(glm::vec2(-1.f, -1.f), view_proj_inv, g_camera_position);
+            raytracing_shader.SetVec3("u_RayDirection00", ray_direction00.x, ray_direction00.y, ray_direction00.z);
+
+            ray_direction01 = GetRayDirectionFromNDC(glm::vec2(-1.f, 1.f), view_proj_inv, g_camera_position);
+            raytracing_shader.SetVec3("u_RayDirection01", ray_direction01.x, ray_direction01.y, ray_direction01.z);
+
+            ray_direction10 = GetRayDirectionFromNDC(glm::vec2(1.f, -1.f), view_proj_inv, g_camera_position);
+            raytracing_shader.SetVec3("u_RayDirection10", ray_direction10.x, ray_direction10.y, ray_direction10.z);
+
+            ray_direction11 = GetRayDirectionFromNDC(glm::vec2(1.f, 1.f), view_proj_inv, g_camera_position);
+            raytracing_shader.SetVec3("u_RayDirection11", ray_direction11.x, ray_direction11.y, ray_direction11.z);
+
+            // Bind level 0 of the framebuffer texture to image binding point 0
+            glBindImageTexture(0, fb_texture, 0, GL_FALSE, 0, GL_WRITE_ONLY, GL_RGBA32F);
+
+            // Dispatch the compute shader to generate a frame in the framebuffer image
+            glDispatchCompute(g_width / work_group_size[0], g_height / work_group_size[1], 1);
+
+            // Unbind image binding point
+            glBindImageTexture(0, 0, 0, GL_FALSE, 0, GL_WRITE_ONLY, GL_RGBA32F);
+
+            glActiveTexture(GL_TEXTURE0);
+            glBindTexture(GL_TEXTURE_2D, fb_texture);
+
+            fullscreen_quad_shader.Bind();
+            fullscreen_quad.BindVAO();
+            glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        }
 
         glfwSwapBuffers(window);
     }
 
-    glDeleteVertexArrays(1, &quad_vao);
-    glDeleteBuffers(1, &quad_vbo);
-    glDeleteBuffers(1, &quad_ibo);
     glDeleteTextures(1, &fb_texture);
+    glDeleteTextures(1, &image_tex);
 
     glfwTerminate();
     return 0;
--- Ogle/Source/Shaders/FullscreenQuadShader.frag
+++ Ogle/Source/Shaders/FullscreenQuadShader.frag
@@ -8,6 +8,5 @@ uniform sampler2D u_Framebuffer;
 
 void main()
 {
-	// FragColor = vec4(0.8f, 0.3f, 0.2f, 1.f);
 	FragColor = texture(u_Framebuffer, tex_coords);
 }
