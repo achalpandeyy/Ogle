9a4a5f41a52e6436b7519b82e1e8f00471ff4f5e
achalpandeyy <pandeyachal.ap@gmail.com>
2021-01-15 19:13:44 +0530

    VertexBuffer, IndexBuffer and VertexArray instead of a single Mesh class

--- Source/Mesh.cpp
+++ Source/Mesh.cpp
@@ -2,6 +2,56 @@
 
 namespace Ogle
 {
+VertexBuffer::VertexBuffer(void* vertices, size_t vertices_size)
+{
+    glGenBuffers(1, &id);
+    Bind();
+    glBufferData(GL_ARRAY_BUFFER, vertices_size, vertices, GL_STATIC_DRAW);
+    Unbind();
+}
+
+VertexBuffer::~VertexBuffer()
+{
+    glDeleteBuffers(1, &id);
+}
+
+IndexBuffer::IndexBuffer(void* indices, size_t indices_size)
+{
+    glGenBuffers(1, &id);
+    Bind();
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices_size, indices, GL_STATIC_DRAW);
+    Unbind();
+}
+
+IndexBuffer::~IndexBuffer()
+{
+    glDeleteBuffers(1, &id);
+}
+
+VertexArray::VertexArray(VertexBuffer* vbo, IndexBuffer* ibo, VertexAttribs* attribs, GLuint attrib_count, GLsizei stride)
+{
+    glGenVertexArrays(1, &id);
+
+    Bind();
+    vbo->Bind();
+    ibo->Bind();
+
+    for (GLuint i = 0; i < attrib_count; ++i)
+    {
+        glVertexAttribPointer(i, attribs[i].dims, GL_FLOAT, GL_FALSE, stride, (const GLvoid*)attribs[i].offset);
+        glEnableVertexAttribArray(i);
+    }
+
+    vbo->Unbind();
+    ibo->Unbind();
+    Unbind();
+}
+
+VertexArray::~VertexArray()
+{
+    glDeleteVertexArrays(1, &id);
+}
+
 Mesh::Mesh(const float* vertices, unsigned int vertex_count, const unsigned int* indices, unsigned int index_count)
 {
     glGenVertexArrays(1, &vao);
--- Source/Mesh.h
+++ Source/Mesh.h
@@ -4,6 +4,49 @@
 
 namespace Ogle
 {
+struct VertexBuffer
+{
+    VertexBuffer(void* vertices, size_t vertices_size);
+    ~VertexBuffer();
+
+    inline void Bind() const { glBindBuffer(GL_ARRAY_BUFFER, id); }
+    inline void Unbind() const { glBindBuffer(GL_ARRAY_BUFFER, 0); }
+
+private:
+    GLuint id;
+};
+
+struct IndexBuffer
+{
+    IndexBuffer(void* indices, size_t indices_size);
+    ~IndexBuffer();
+
+    inline void Bind() const { glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, id); }
+    inline void Unbind() const { glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0); }
+
+private:
+    GLuint id;
+};
+
+struct VertexAttribs
+{
+    GLint dims;
+    uint64_t offset;
+};
+
+struct VertexArray
+{
+    VertexArray(VertexBuffer* vbo, IndexBuffer* ibo, VertexAttribs* attribs, GLuint attrib_count, GLsizei stride);
+    ~VertexArray();
+
+    inline void Bind() const { glBindVertexArray(id); }
+    inline void Unbind() const { glBindVertexArray(0); }
+
+private:
+    GLuint id;
+};
+
+// Todo: Make Mesh use the aforementioned classes, or do we need Mesh at all?
 struct Mesh
 {
     Mesh(const float* vertices, unsigned int vertex_count, const unsigned int* indices, unsigned int index_count);
